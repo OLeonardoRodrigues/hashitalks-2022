7ca189f409062632e3424ec73e752114edf5bf57
OLeonardoRodrigues <oliveira.leonardo.r@gmail.com>
2022-02-11 17:23:38 -0300

    new version

--- subnets.tf
+++ subnets.tf
@@ -1,10 +1,10 @@
 resource "aws_subnet" "this" {
-  count = length(var.public_subnets)
+  count       = length(var.public_subnets)
 
-  vpc_id                          = aws_vpc.this.id
-  cidr_block                      = element(concat(var.public_subnets, [""]), count.index)
+  vpc_id      = aws_vpc.this.id
+  cidr_block  = element(concat(var.public_subnets, [""]), count.index)
 
-  tags = merge(
+  tags        = merge(
     {
       "Name" = format(
         "${var.name}-subnet-${count.index}"
